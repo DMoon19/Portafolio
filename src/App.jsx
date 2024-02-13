9f8b38993cd3dd4533fb31526747897bf43b2abb
David García <81989868+DMoon19@users.noreply.github.com>
2024-02-13 06:10:41 +0000

    Cambios template

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "David Garcia",
+  title: "Web Designer & Game Dev",
+  email: "david.garciasan@upb.edu.co",
+  gitHub: "DMoon19",
+  instagram: "dmoon19.png",
+  linkedIn: "david-garcia-sanchez-2baa6b1ba",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "DMoon19_",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
